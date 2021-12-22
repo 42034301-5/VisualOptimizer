2de68918cfd62d417168366571be7b326a6265d6
Letian <54671931+li-letian@users.noreply.github.com>
2021-12-22 16:11:00 +0800

    锅砸了重搞

---
+++ CodeOptimizer/CodeOptimizer.h
@@ -0,0 +1,58 @@
+﻿// CodeOptimizer.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <random>
+
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+
+#include <cpprest/http_listener.h>              // HTTP server
+#include <cpprest/json.h>                       // JSON library
+#include <cpprest/uri.h>                        // URI library
+#include <cpprest/ws_client.h>                  // WebSocket client
+#include <cpprest/containerstream.h>            // Async streams backed by STL containers
+#include <cpprest/interopstream.h>              // Bridges for integrating Async streams with STL and WinRT streams
+#include <cpprest/rawptrstream.h>               // Async streams backed by raw pointer to memory
+#include <cpprest/producerconsumerstream.h>     // Async streams for producer consumer scenarios
+
+// TODO: 在此处引用程序需要的其他标头。
+
+
+using namespace std;
+using namespace utility;                    // Common utilities like string conversions
+using namespace web;                        // Common features like URIs.
+using namespace web::http;                  // Common HTTP functionality
+using namespace concurrency::streams;       // Asynchronous streams
+using namespace web::http::experimental::listener;          // HTTP server
+using namespace web::json;   // JSON library
+
+
+class handler
+{
+public:
+    handler();
+    handler(utility::string_t url);
+    virtual ~handler();
+
+    pplx::task<void>open() { return m_listener.open(); }
+    pplx::task<void>close() { return m_listener.close(); }
+
+protected:
+
+private:
+    void handle_get(http_request message);
+    void handle_put(http_request message);
+    void handle_post(http_request message);
+    void handle_delete(http_request message);
+    void handle_error(pplx::task<void>& t);
+    http_listener m_listener;
+};
---
+++ CodeOptimizer/main.cpp
@@ -0,0 +1,43 @@
+
+#include "CodeOptimizer.h"
+
+
+static std::unique_ptr<handler> g_httpHandler;
+
+void on_initialize(const string_t& address)
+{
+
+
+    uri_builder uri(address);
+
+    auto addr = uri.to_uri().to_string();
+    g_httpHandler = std::unique_ptr<handler>(new handler(addr));
+    g_httpHandler->open().wait();
+
+    ucout << utility::string_t(U("Listening for requests at: ")) << addr << std::endl;
+
+    return;
+}
+
+void on_shutdown()
+{
+    g_httpHandler->close().wait();
+    return;
+}
+
+
+int main(int argc, char* argv[])
+{
+    utility::string_t port = U("8081");
+    utility::string_t address = U("http://localhost:");
+    address.append(port);
+
+    on_initialize(address);
+    std::cout << "Press ENTER to exit." << std::endl;
+
+    std::string line;
+    std::getline(std::cin, line);
+
+    on_shutdown();
+    return 0;
+}
