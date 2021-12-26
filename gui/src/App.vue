5b46140886325bf0b803ae3f6d39bcef2f6192d7
Letian <54671931+li-letian@users.noreply.github.com>
2021-12-26 17:47:19 +0800

    调了调样式

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <router-view/>
+      <div class="back" :style="conTop"></div>
+      <router-view />
   </div>
 </template>
 
@@ -10,16 +11,35 @@ export default {
   created:function()
   {
       this.$router.push({name:"GraphView"});
+  },
+  data()
+  {
+      return {
+          conTop:{
+            backgroundImage:'url(' + require('@/assets/background.jpg') + ')',
+            backgroundRepeat:'repeat',
+            backgroundSize: "cover",
+            
+          }
+      }
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: Helvetica, Arial, sans-serif;
+  font-family: Helvetica,Verdana, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.back{
+    position: absolute;
+    top:0px;
+    height: 1000px;
+    width: 100%;
+    z-index: -10;
 }
 </style>
